709ba38e64e5d51dc2a1fddd3bb9a5e1c41833c1
Valentin Virot <virot.valentin@gmail.com>
2021-01-22 16:47:08 +0100

    Created Base Angular App + navbar done

---
+++ leadon-portfolio/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeSectionComponent } from './home-section/home-section.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeSectionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

eb5ea9ebae9f9e203d1ca84b3cecf090e9d877dc
Valentin Virot <virot.valentin@gmail.com>
2023-08-01 14:06:52 +0200

    Added dynamic data binding on players.

--- src/app/fifa/components/players/fifa-players.component.ts
+++ src/app/fifa/components/players/fifa-players.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { Player } from 'src/app/classes/Player';
 
 @Component({
@@ -6,19 +7,19 @@ import { Player } from 'src/app/classes/Player';
   templateUrl: './fifa-players.component.html',
   styleUrls: ['./fifa-players.component.scss'],
 })
-export class FifaPlayersComponent {
-  players: Player[] = [
-    new Player(
-      'Valentin VIROT',
-      'https://media.licdn.com/dms/image/D4E03AQGpk_EvdcLi1w/profile-displayphoto-shrink_800_800/0/1684249003276?e=1696464000&v=beta&t=0SYFAz3I2F0xeEtwv0901-XjHmxGVdkq-SfJbER3PK4'
-    ),
-    new Player(
-      'Valentyn NOËL',
-      'https://media.licdn.com/dms/image/D4E03AQHS8j5PWwDyfg/profile-displayphoto-shrink_800_800/0/1670626112878?e=1696464000&v=beta&t=FwC9dQPekuTT56q5fk30KBQyG4EPT4MaIVs5e9CQI2I'
-    ),
-    new Player(
-      'Rémi MARCIANO',
-      'https://media.licdn.com/dms/image/C4D03AQFhdqIv1qctkg/profile-displayphoto-shrink_800_800/0/1613575702887?e=1696464000&v=beta&t=Dy5QpJNisF6BT_BBQsbyrxo0foiS_MAPN1ejHELz5s4'
-    ),
-  ];
+export class FifaPlayersComponent implements OnInit {
+  players: Player[] = [];
+
+  constructor(private client: HttpClient) {}
+
+  ngOnInit(): void {
+    console.log('Oui');
+    this.client
+      .get(
+        'https://raw.githubusercontent.com/LeadOn/Database/master/players.json'
+      )
+      .subscribe((data) => {
+        this.players = data as Player[];
+      });
+  }
 }

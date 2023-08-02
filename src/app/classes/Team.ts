a327911f0a1f83639341c8c895613aa50d25c744
Valentin Virot <virot.valentin@gmail.com>
2023-08-02 18:38:51 +0200

    Working on YuFoot API integration.

---
+++ src/app/classes/Team.ts
@@ -0,0 +1,8 @@
+import { Player } from './Player';
+
+export class Team {
+  id: number = 0;
+  code: string = 'ASM';
+  score: number = 0;
+  players: Player[] = [];
+}
---
+++ src/app/services/yufoot-api/yufoot-api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Player } from 'src/app/classes/Player';
+import { GamePlayed } from 'src/app/classes/GamePlayed';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class YuFootApiService {
+  baseUrl = 'http://localhost:5184';
+
+  constructor(private client: HttpClient) {}
+
+  getPlayers(): Observable<Player[]> {
+    return this.client.get<Player[]>(this.baseUrl + '/player');
+  }
+
+  getLastFiveGames(): Observable<GamePlayed[]> {
+    return this.client.get<GamePlayed[]>(this.baseUrl + '/game/last');
+  }
+}

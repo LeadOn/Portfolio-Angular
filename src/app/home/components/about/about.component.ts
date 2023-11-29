47ca11cb8133859d51ebfb69a9ddd70633e4f276
Valentin Virot <virot.valentin@gmail.com>
2023-11-29 21:06:01 +0100

    Fixed tiny design flaws

--- src/app/home/components/about/about.component.ts
+++ src/app/home/components/about/about.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class HomeAboutComponent {}
+export class HomeAboutComponent {
+  infoIcon = faInfoCircle;
+}
